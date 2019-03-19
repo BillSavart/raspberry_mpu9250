12db798285be5b721f3b1189906cde1098070769
BillSavart <energyy0512@gmail.com>
2019-03-19 21:14:18 +0800

    move new new

--- bes.py
+++ bes.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python
 import smbus
 import math
 import time
- 
+from scipy import signal
+
 # Register
 power_mgmt_1 = 0x6b
 power_mgmt_2 = 0x6c
@@ -23,7 +23,7 @@ def read_word_2c(reg, addr):
     else:
         return val
 
-# 1:320
+# 1:320 for map information
 bus = smbus.SMBus(1) # bus = smbus.SMBus(0) fuer Revision 1
 address = 0x68       # via i2cdetect
 bus.write_byte_data(address, power_mgmt_1, 0)  #initiate mpu6500
@@ -32,73 +32,54 @@ bus.write_byte_data(address, 0x37, 0x02) #initiate bypass
 mag_address = 0x0c
 bus.write_byte_data(mag_address, 0x0A, 0b0110) #initiate AKB8963
 
-#record_f = open('record.txt','w')  #the data of bes
-#time_f = open('time.txt','w') #the data of bes time
-#mag_record_f = open('mag_record.txt', 'w') #the data of mag
-#mag_time_f = open('mag_time.txt', 'w') #the data of mag time
+# list for low pass filter
+bes_arr = []
+time_arr = []
 
-t = 0
+#
+min_num = -0.5
+max_num = 0.5
+sample_num = 500
 
-# Aktivieren, um das Modul ansprechen zu koennen
-#bus.write_byte_data(address, power_mgmt_1, 0)
+# low pass filter 
+order = 3
+Wn = 0.003
+b,a = signal.butter(order, Wn, 'low')
 
 distance = 0
-speed = 0
-start = 0 #bes time
-prev = 0 #mag data
-now = 0 #mag data
-count_mag_time = 0 #mag delay
+velocity = 0
+
+velocity_f = open("vfile.txt","w")
 
 while True:
-    beschleunigung_xout = read_word_2c(0x3b, address)
-    beschleunigung_yout = read_word_2c(0x3d, address)
-    beschleunigung_zout = read_word_2c(0x3f, address)
+    time_temp = time.time()
+    time_arr.append(time_temp)
 
-    beschleunigung_yout_skaliert = beschleunigung_yout / 16384.0 * 9.8
+    bes_x = read_word_2c(0x3b, address)
+    bes_y = read_word_2c(0x3d, address)
+    bes_z = read_word_2c(0x3f, address)
 
-    mag_xout = read_word_2c(0x04, mag_address)
-    mag_yout = read_word_2c(0x06, mag_address)
-    mag_zout = read_word_2c(0x08, mag_address)
+    bes_y_ska = bes_y / 16384.0 * 9.8
 
-    if start == 0:
-        start = time.time()
-        prev = mag_yout
-
-    end = time.time()  
-    time_interval = end - start
-
-    count_mag_time = count_mag_time + 1
-    if count_mag_time == 700:
-        count_mag_time = 0
-        now = mag_yout
-        #count if stop
-        if prev - now > -5 and prev - now < 5:
-            #print "no move"
-            distance = 0
-            speed = 0
+    if len(bes_arr) == sample_num:
+        lfilt = signal.lfilter(b, a, bes_arr)
+        lfilt = signal.lfilter(b, a, lfilt)
+        if max(lfilt) < max_num and min(lfilt) > min_num:
+            pass
         else:
-            print "distance:",distance
-            #distance = 0
-            #the bes value is valid
-        prev = now
-
-    if beschleunigung_yout_skaliert < 1 and beschleunigung_yout_skaliert > -1:
-        pass
+            i = 1
+            while i < sample_num:
+                if lfilt[i] < max_num and lfilt[i] > min_num:
+                    pass
+                else:
+                    distance = distance + velocity*(time_arr[i]-time_arr[i-1])+0.5*lfilt[i]*(time_arr[i]-time_arr[i-1])*(time_arr[i]-time_arr[i-1])
+                    velocity = velocity + lfilt[i]*(time_arr[i]-time_arr[i-1])
+                i = i + 1
+        bes_arr = []
+        time_arr = []
+        print "distance: " , distance
+        #velocity_f.write(str(velocity))
+        #velocity_f.write(" ")
     else:
-        distance  = distance + speed*time_interval + 0.5*beschleunigung_yout_skaliert*time_interval*time_interval
-        speed = speed + (time_interval * beschleunigung_yout_skaliert)
-        #print "distance: ", distance
-        #print "beschleunigung_yout: ", beschleunigung_yout_skaliert
-        #print "speed: ", speed
-
-    #s = str(beschleunigung_yout_skaliert)
-    #record_f.write(s)
-    #record_f.write(' ')
+        bes_arr.append(bes_y_ska)
         
-    #t = t + time_interval
-    #t1 = str(t)
-    #time_f.write(t1)
-    #time_f.write(' ')
-        
-    start = end
-    prev = now
