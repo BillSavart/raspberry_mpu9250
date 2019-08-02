c08028a6204821653750daf6fb5f2a07a0ca3825
Bill Wang <energyy0512@gmail.com>
2019-08-02 21:24:51 +0800

    plot

--- raspberry_pi/MultiProcess/plot.py
+++ raspberry_pi/MultiProcess/plot.py
@@ -6,9 +6,9 @@ count = 1
 plt.figure(1)
 temp = 0
 while count <= 98:
-    stop_f = np.loadtxt("stop_chair.txt")
-    walk_f = np.loadtxt("walk_chair.txt")
-    #run_f = np.loadtxt("run" + str(count) + ".txt")
+    stop_f = np.loadtxt("left_waist.txt")
+    walk_f = np.loadtxt("no_turn_waist.txt")
+    run_f = np.loadtxt("right_waist.txt")
 
     #y_stop = signal.lfilter(b,a,stop_f)
     #y_walk = signal.lfilter(b,a,walk_f)
@@ -19,17 +19,20 @@ while count <= 98:
     #std_r = np.std(y_run)
     tmp = []
     tmp_walk = []
+    tmp_run = []
     while temp < 500:
         tmp.append(stop_f[temp*count])
         tmp_walk.append(walk_f[temp*count])
+        tmp_run.append(run_f[temp*count])
         std_s = np.std(tmp)
         std_w = np.std(tmp_walk)
+        std_r = np.std(tmp_run)
   #      print(temp)
         temp = temp + 1
 
     plt.scatter(std_s, 0, color='blue')
     plt.scatter(std_w, 0, color='red')
-    #plt.scatter(std_r, 0, color='green')
+    plt.scatter(std_r, 0, color='green')
 
     count = count + 1
     temp = 0
